b8bf10896628ff5e6f14f6ee30b54a540c439560
Daragh Hollman <hollmandaragh@gmail.com>
2024-12-09 10:51:04 +0000

    Updated grazing angle calculation to match what is in hermpy/traj.py

--- create_sample_features.py
+++ create_sample_features.py
@@ -7,11 +7,11 @@ import multiprocessing
 
 import diptest
 import hermpy.trajectory as traj
-from hermpy.utils import User, Constants
 import numpy as np
 import pandas as pd
 import scipy.stats
 import spiceypy as spice
+from hermpy.utils import Constants
 
 
 def main():
@@ -26,7 +26,10 @@ def main():
         parse_dates=["Crossing Start", "Crossing End"],
     )
 
-    outputs = ["/home/daraghhollman/Main/Work/mercury/DataSets/magnetosheath_features.csv", "/home/daraghhollman/Main/Work/mercury/DataSets/solar_wind_features.csv"]
+    outputs = [
+        "/home/daraghhollman/Main/Work/mercury/DataSets/magnetosheath_features.csv",
+        "/home/daraghhollman/Main/Work/mercury/DataSets/solar_wind_features.csv",
+    ]
 
     for samples_data_set, label, output in zip(
         [ms_samples_data_set, sw_samples_data_set], ["MS", "SW"], outputs
@@ -87,9 +90,7 @@ def Get_Features(input):
         mean = np.mean([row["|B|"], row["Bx"], row["By"], row["Bz"]], axis=1)
         median = np.median([row["|B|"], row["Bx"], row["By"], row["Bz"]], axis=1)
         std = np.std([row["|B|"], row["Bx"], row["By"], row["Bz"]], axis=1)
-        skew = scipy.stats.skew(
-            [row["|B|"], row["Bx"], row["By"], row["Bz"]], axis=1
-        )
+        skew = scipy.stats.skew([row["|B|"], row["Bx"], row["By"], row["Bz"]], axis=1)
         kurtosis = scipy.stats.kurtosis(
             [row["|B|"], row["Bx"], row["By"], row["Bz"]], axis=1
         )
@@ -103,16 +104,23 @@ def Get_Features(input):
 
         grazing_angle = Get_Grazing_Angle(row)
 
+        try:
+            crossing_start = dt.datetime.strptime(
+                row["Crossing Start"], "%Y-%m-%d %H:%M:%S.%f"
+            )
+        except:
+            crossing_start = dt.datetime.strptime(
+                row["Crossing Start"], "%Y-%m-%d %H:%M:%S"
+            )
 
         try:
-            crossing_start = dt.datetime.strptime(row["Crossing Start"], "%Y-%m-%d %H:%M:%S.%f")
+            sample_start = dt.datetime.strptime(
+                row["Sample Start"], "%Y-%m-%d %H:%M:%S.%f"
+            )
         except:
-            crossing_start = dt.datetime.strptime(row["Crossing Start"], "%Y-%m-%d %H:%M:%S")
-
-        try:
-            sample_start = dt.datetime.strptime(row["Sample Start"], "%Y-%m-%d %H:%M:%S.%f")
-        except:
-            sample_start = dt.datetime.strptime(row["Sample Start"], "%Y-%m-%d %H:%M:%S")
+            sample_start = dt.datetime.strptime(
+                row["Sample Start"], "%Y-%m-%d %H:%M:%S"
+            )
 
         if label == "SW":
             # Solar wind sample is before the crossing
@@ -134,7 +142,6 @@ def Get_Features(input):
 
         else:
             raise Exception(f"Unknown sample label: {label}, expected 'SW' or 'MS'")
-
 
     finally:
         spice.kclear()
@@ -171,7 +178,7 @@ def Get_Grazing_Angle(row, function="bow shock"):
     Assuming any expansion / compression occurs parallel to the normal vector
     of the curve, the vector to the closest point on the bow shock to MESSENGER
     is parallel with the bow shock normal at that closest point
-    
+
     We then compare this vector, with the velocity vector of the spacecraft.
     """
     try:
@@ -180,27 +187,43 @@ def Get_Grazing_Angle(row, function="bow shock"):
     except:
         start_time = dt.datetime.strptime(row["Crossing Start"], "%Y-%m-%d %H:%M:%S")
 
-    start_position = traj.Get_Position(
-        "MESSENGER",
-        start_time,
-        frame="MSM",
-        aberrate=True,
-    ) / Constants.MERCURY_RADIUS_KM
+    try:
+        end_time = dt.datetime.strptime(row["Crossing End"], "%Y-%m-%d %H:%M:%S.%f")
 
-    next_position = traj.Get_Position(
-        "MESSENGER",
-        start_time + dt.timedelta(seconds=1),
-        frame="MSM",
-        aberrate=True,
-    ) / Constants.MERCURY_RADIUS_KM
+    except:
+        end_time = dt.datetime.strptime(row["Crossing End"], "%Y-%m-%d %H:%M:%S")
 
-    cylindrical_start_position = np.array([start_position[0], np.sqrt(start_position[1] ** 2 + start_position[2] ** 2)])
-    cylindrical_next_position = np.array([next_position[0], np.sqrt(next_position[1] ** 2 + next_position[2] ** 2)])
+    start_position = (
+        traj.Get_Position(
+            "MESSENGER",
+            start_time + (end_time - start_time) / 2,
+            frame="MSM",
+            aberrate=True,
+        )
+        / Constants.MERCURY_RADIUS_KM
+    )
+
+    next_position = (
+        traj.Get_Position(
+            "MESSENGER",
+            start_time + (end_time - start_time) / 2 + dt.timedelta(seconds=1),
+            frame="MSM",
+            aberrate=True,
+        )
+        / Constants.MERCURY_RADIUS_KM
+    )
+
+    cylindrical_start_position = np.array(
+        [start_position[0], np.sqrt(start_position[1] ** 2 + start_position[2] ** 2)]
+    )
+    cylindrical_next_position = np.array(
+        [next_position[0], np.sqrt(next_position[1] ** 2 + next_position[2] ** 2)]
+    )
 
     cylindrical_velocity = cylindrical_next_position - cylindrical_start_position
 
     # normalise velocity
-    cylindrical_velocity /= np.sqrt( np.sum(cylindrical_velocity ** 2) )
+    cylindrical_velocity /= np.sqrt(np.sum(cylindrical_velocity**2))
 
     match function:
         case "bow shock":
@@ -218,9 +241,7 @@ def Get_Grazing_Angle(row, function="bow shock"):
             bow_shock_x_coords = initial_x + rho * np.cos(phi)
             bow_shock_r_coords = rho * np.sin(phi)
 
-            boundary_positions = np.array(
-                [bow_shock_x_coords, bow_shock_r_coords]
-            ).T
+            boundary_positions = np.array([bow_shock_x_coords, bow_shock_r_coords]).T
 
         case "magnetopause":
             # Winslow+ (2013) parameters
@@ -240,33 +261,38 @@ def Get_Grazing_Angle(row, function="bow shock"):
             ).T
 
         case _:
-            raise ValueError( f"Invalid function choice: {function}. Options are 'bow shock', 'magnetopause'.")
+            raise ValueError(
+                f"Invalid function choice: {function}. Options are 'bow shock', 'magnetopause'."
+            )
 
-
-    # This is setup is faster than iterrating through the points.
+    # We need to determine which point on the boundary curve is closest to the spacecraft
+    # This method, utilising a k-d tree is computationally faster than iterrating through
+    # the points and determining the distance.
     # O(logN) vs O(N)
     kd_tree = scipy.spatial.KDTree(boundary_positions)
     
-    _, index = kd_tree.query(cylindrical_start_position)
-    closest_position = index
+    _, closest_position = kd_tree.query(cylindrical_start_position)
 
     # Get the normal vector of the BS at this point
-    # This is just the normalised vector between the spacecraft and the closest point
+    # This is just the normalised vector between the spacecraft and the closest point,
+    # as the vector between an arbitrary point and the closest point on an arbitrary
+    # curve is parallel to the normal vector of that curve at that closest point.
     normal_vector = boundary_positions[closest_position] - cylindrical_start_position
 
     normal_vector = normal_vector / np.sqrt(np.sum(normal_vector**2))
 
     grazing_angle = np.arccos(
                         np.dot(normal_vector, cylindrical_velocity)
-                        / (np.sqrt(np.sum(normal_vector**2)) + np.sqrt( np.sum(cylindrical_velocity ** 2) ))
+                        / (np.sqrt(np.sum(normal_vector**2)) * np.sqrt( np.sum(cylindrical_velocity ** 2) ))
                     )
     grazing_angle = Constants.RADIANS_TO_DEGREES(grazing_angle)
 
     # If the grazing angle is greater than 90, then we take 180 - angle as its from the other side
     # This occurs as we don't make an assumption as to what side of the model boundary we are.
     # i.e. we could be referencing the normal, or the anti-normal.
-
     if grazing_angle > 90:
+        # If the angle is greater than 90 degrees, we have the normal vector
+        # the wrong way around. i.e. the inward pointing normal.
         grazing_angle = 180 - grazing_angle
 
     return grazing_angle
